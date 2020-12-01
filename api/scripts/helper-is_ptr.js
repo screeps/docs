610e0f0943edf7f4f837ddef4cc8af961545726b
Artem Chivchalov <chivchalov@gmail.com>
2020-12-01 19:57:04 +0300

    ✨ add is_season helper

--- api/scripts/helper-is_ptr.js
+++ api/scripts/helper-is_ptr.js
@@ -2,3 +2,7 @@
 hexo.extend.helper.register('is_ptr', function() {
     return process.env.IS_PTR === "1";
 });
+
+hexo.extend.helper.register('is_season', function() {
+    return process.env.IS_SEASON === "1";
+});
